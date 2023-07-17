df07c35410861ab3600b0654d77c5f6d49c649d2
SamarV-121 <samar@samarv121.dev>
2023-07-17 19:39:27 +0000

    RM6785: Update target devices
    Change-Id: Ia6b8eccd3998e9514f48fb60aed8dc74b5a1f607

--- RM6785/firmware.mk
+++ RM6785/firmware.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter RMX2001 RMX2151,$(TARGET_DEVICE)),)
+ifneq ($(filter RMX2001L1 RMX2151L1,$(TARGET_DEVICE)),)
 
 RADIO_FILES := $(wildcard $(LOCAL_PATH)/radio/*)
 $(foreach f, $(notdir $(RADIO_FILES)), \
